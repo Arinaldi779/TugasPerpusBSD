30ae076d3cc05ec196ab073e96d638ea0e6d01a0
Arinaldi <arinaldisurya374@gmail.com>
2024-12-07 04:10:43 +0800

    buat tampilan login dan sedikit dashboard, masih html

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./views/**/*.{html,js}", // Semua file HTML dan JS di dalam folder views
+    "./src/**/*.{html,js}", // Jika ada file HTML atau JS dalam folder src
+  ],
+  theme: {
+    extend: {
+      colors: {
+        color1: "##fcd34d",
+        color2: "##fef9c3",
+        color3: "#FAFFAF",
+        primary: "#3FA2F6",
+      },
+    },
+  },
+  plugins: [],
+};
